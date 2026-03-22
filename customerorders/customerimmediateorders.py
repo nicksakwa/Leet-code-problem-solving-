51d0878b95cbd6eec29160962a6b5a12e6870539
nicksakwa <nsakwa@gmail.com>
2026-03-22 23:21:25 +0300

    customer immediate order pandas

---
+++ customerorders/customerimmediateorders.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    # Fix: Added the underscore to 'customer_id'
+    first_orders = delivery.sort_values(['customer_id', 'order_date']).drop_duplicates('customer_id')
+    
+    is_immediate = first_orders['order_date'] == first_orders['customer_pref_delivery_date']
+    percentage = round(is_immediate.mean() * 100, 2)
+    
+    return pd.DataFrame({'immediate_percentage':[percentage]})
