30f7eb3b47a8c957c7492647d243d2a85bcb16f6
nicksakwa <nsakwa@gmail.com>
2026-05-05 11:25:08 +0300

    productorder units solution

---
+++ salesproducts/productsordersunits.sql
@@ -0,0 +1,13 @@
+SELECT 
+    Products.product_name,
+    SUM(Orders.unit) AS unit
+FROM Products
+INNER JOIN Orders
+    ON Products.product_id = Orders.product_id
+WHERE
+    Orders.order_date >= '2020-02-01'
+    AND Orders.order_date <= '2020-02-29'
+GROUP BY
+    Products.product_id
+HAVING
+    SUM(Orders.unit) >= 100;
