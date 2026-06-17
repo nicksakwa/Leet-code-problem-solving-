47d46b34436306fe96c03c0c02b3af50c30262ef
nicksakwa <nsakwa@gmail.com>
2026-06-17 12:56:34 +0300

    capital gainssimplified SQL solution

--- capital-gains/capitalgains.sql
+++ capital-gains/capitalgains.sql
@@ -1,10 +1,10 @@
 SELECT
-    Stocks.stock_name,
+    stock_name,
     SUM(
         CASE
-            WHEN Stocks.operation ='Buy' Then -Stocks.price
-            ELSE Stocks.price
+            WHEN operation ='Buy' Then -price
+            ELSE price
         END
     ) AS capital_gain_loss
 FROM Stocks
-GROUP BY Stocks.stock_name;
+GROUP BY stock_name;
